d9e4c4ecaaaadf039e49c7a9afcd4cd323834aa5
Tom Brennan <tjb1982@gmail.com>
2013-11-11 16:30:51 -0500

    bulk

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = function cmOpenid (providers) {
+module.exports = function cmOpenid (context) {
+  context = context || {};
+  var redirect_uri = context.redirect_uri;
   return (
     ["div",
       {class:"cm cm-openid"},
-      providers.map(function(p) {
-        return (["a", {"href": p.authURI }, p.name]);
+      context.providers.map(function(p) {
+        return (["a", {"href": p.authURI + (context.query || '')}, p.name]);
       })]
   );
 };
